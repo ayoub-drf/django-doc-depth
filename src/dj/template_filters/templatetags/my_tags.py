41b83f2c983cd5405c9ba43e2d02b77b1b4f6c57
x <x@aol.com>
2025-01-06 23:52:25 +0100

    custom tags and filters in django template

--- src/dj/template_filters/templatetags/my_tags.py
+++ src/dj/template_filters/templatetags/my_tags.py
@@ -1,19 +1,36 @@
-# myapp/templatetags/my_tags.py
 from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import SafeString
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 
 register = template.Library()
+
+
+@register.filter(needs_autoescape=True)
+def initial_letter_filter(text, autoescape=True):
+    first, other = text[0], text[1:]
+    if autoescape:
+        esc = conditional_escape
+    else:
+        esc = lambda x: x
+    result = "<strong>%s</strong>%s" % (esc(first), esc(other))
+    return mark_safe(result)
+
+
+@register.filter(name="cut", is_safe=True)
+def cut(value, arg):
+    return value.replace(arg, "")
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def addUpper(value):
+    print("SafeString", isinstance(value, SafeString))
+    return value.upper()
+
 
 @register.simple_tag
 def add_numbers(a, b):
     return a + b
 
-@register.simple_tag
-def cut(value, arg):
-    """Removes all values of arg from the given string"""
-    return value.replace(arg, "")
-
-register.filter("cut", cut)
-
-@register.simple_tag
-def add_upper(v):
-    return v.upper()
