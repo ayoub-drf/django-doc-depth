c24dfcafe46b7b1fde5d92d99780862638769cf4
ayoub <ayoubx0x9@gmail.com>
2025-01-27 21:02:22 -0500

    custom commands in django

---
+++ main.py
@@ -0,0 +1,11 @@
+# PYTHONPATH=/home/x/Desktop/django-doc-depth django-admin runserver --settings=dj.settings
+# python -m django runserver --settings=dj.settings
+
+# django.core.management.call_command(name, *args, **options)
+
+# from django.core import management
+# from django.core.management.commands import loaddata
+
+# management.call_command("flush", verbosity=0, interactive=False)
+# management.call_command("loaddata", "test_data", verbosity=0)
+# management.call_command(loaddata.Command(), "test_data", verbosity=0)
