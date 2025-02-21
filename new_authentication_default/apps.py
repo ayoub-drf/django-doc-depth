14fe94f7083031d94da35a7572f618654d876f9a
ayoub <ayoubx0x9@gmail.com>
2025-02-20 21:28:02 -0500

    handling timezones in django

---
+++ new_authentication_default/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NewAuthenticationDefaultConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'new_authentication_default'
